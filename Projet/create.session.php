b2a5d8ff02a41ae85e5c19c7c8500d4b0c31510f
MatthieuALLIER <81558595+MatthieuALLIER@users.noreply.github.com>
2021-04-17 14:50:33 +0200

    MAJ

--- Projet/create.session.php
+++ Projet/create.session.php
@@ -8,12 +8,9 @@
 	include("entete.sup.html");
 	echo "Connexion";
 	include("entete.inf.html");
-	if ($_SESSION['connection_error']) {
-		$gab = new Template("./");
-		$gab->set_filenames(array("body" => "error.connection.message.html"));	
-		$gab->assign_vars(array("error_message" => $_SESSION['connection_message']));
-		$gab->pparse("body");
-	}
-	include("form.session.html");
+	$gab = new Template("./");
+	$gab->set_filenames(array("body" => "form.session.html"));	
+	$gab->assign_vars(array("error_message" => $_SESSION['connection_message']));
+	$gab->pparse("body");
 	include("pied.inc.html");
 ?>
--- Projet/deconnect.session.php
+++ Projet/deconnect.session.php
@@ -3,7 +3,7 @@
 	
 	$_SESSION = array();
 	$_SESSION['connection_error'] = false;
-	
+	$_SESSION['connection_message'] = "";
 	header('Location: create.session.php');
 	exit();
 ?>
