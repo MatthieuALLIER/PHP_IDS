e46de7304d9b2de8b2461cb10b0dd997ea114c31
MatthieuALLIER <81558595+MatthieuALLIER@users.noreply.github.com>
2021-04-18 15:30:32 +0200

    Delete deconnect.session.php

--- deconnect.session.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	
-	$_SESSION = array();
-	$_SESSION['connection_error'] = false;
-	$_SESSION['connection_message'] = "";
-	header('Location: create.session.php');
-	exit();
-?>
